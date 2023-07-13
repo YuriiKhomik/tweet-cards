920a2881de775e2f5ff9de50f777940b9ecb34e2
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-07-13 12:48:52 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/tweet-cards">
+    <BrowserRouter basename="/tweet-cards">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
