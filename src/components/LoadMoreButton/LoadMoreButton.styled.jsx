b79cfeafd50b2b7a7c0a2fd13785b15981e064c9
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-07-15 13:34:45 +0300

    ...

--- src/components/LoadMoreButton/LoadMoreButton.styled.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.styled.jsx
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 export const StyledLMButton = styled.button`
   min-width: 196px;
   height: 50px;
+  padding: 16px 5px;
   border: none;
   border-radius: 10px;
   box-shadow: 0px 3.44px 3.44px rgba(0, 0, 0, 0.25);
 
   cursor: pointer;
-  background-color: #5cd3a8;
+  background-color: #ebd8ff;
   color: #373737;
   font-size: 18px;
   font-weight: 600;
@@ -16,7 +17,7 @@ export const StyledLMButton = styled.button`
 
   &:hover,
   &:focus {
-    border: 2px solid blue;
+    background-color: #5cd3a8;
 
     &:disabled {
       background: #e7e8e9;
